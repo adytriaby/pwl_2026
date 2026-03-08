8ceccfbebb7423486b40288bbf1b3b42b9c98914
adytriaby <aditriyabayuramadani@gmail.com>
2026-03-08 22:54:47 +0700

    Create Laravel POS practical project

---
+++ POS/resources/views/products/food-beverage.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POS - Food Beverage</title>
+</head>
+<body>
+    <h1>Category: Food & Beverage</h1>
+    <p>Menampilkan daftar produk makanan dan minuman.</p>
+</body>
+</html>
---
+++ POS/resources/views/user.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POS - User</title>
+</head>
+<body>
+    <h1>Halaman User</h1>
+    <p>ID User: {{ $id }}</p>
+    <p>Nama User: {{ $name }}</p>
+</body>
+</html>
